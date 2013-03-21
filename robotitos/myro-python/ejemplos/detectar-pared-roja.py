724b8e9553204f3d8d9d37ba3c9e168076887dd3
Rafael Ignacio Zurita <rafa@pampero>
2013-03-21 19:01:53 -0300

    Ejemplo testeado

---
+++ robotitos/myro-python/ejemplos/detectar-pared-roja.py
@@ -0,0 +1,11 @@
+>>> def fun():
+...     ro.forward(.1)
+...     while(1):
+...             faivisionlib.detectar_objetos (1, 1, 160, 120)
+...             a = faivisionlib.objeto_porc(0)
+...             if a > 20 :
+...                     ro.stop()
+...                     break
+...             wait(.5)
+... 
+
