a055677ed83107ef9597cd72428586412d02c747
Rafael Ignacio Zurita <rafa@pampero>
2013-03-22 15:18:38 -0300

    mejoras sustanciales

--- robotitos/myro-python/ejemplos/detectar-pared-roja.py
+++ robotitos/myro-python/ejemplos/detectar-pared-roja.py
@@ -1,11 +1,27 @@
->>> def fun():
-...     ro.forward(.1)
-...     while(1):
-...             faivisionlib.detectar_objetos (1, 1, 160, 120)
-...             a = faivisionlib.objeto_porc(0)
-...             if a > 20 :
-...                     ro.stop()
-...                     break
-...             wait(.5)
-... 
+from myro import *
 
+def fun():
+	ro.forward(.1)
+	movimiento = 1
+	while(1):
+		vision.detectar_objetos (1, 1, 160, 120)
+		a = vision.objeto_porc(0)
+		if a > 20 :
+			ro.stop()
+			wait(3)
+			ro.turnLeft(.3, .5)
+			movimiento = 0
+			wait(3)
+		if movimiento == 0:
+			movimiento = 1
+			ro.forward(.1)
+			
+		wait(.5)
+
+
+import ctypes
+vision = ctypes.CDLL("/usr/local/lib/libfaivision.so")
+vision.faivision_init()
+
+ro = Scribbler("/dev/pts/15")
+fun()
