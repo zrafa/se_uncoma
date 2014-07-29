11bb0b2ea56c616c957d3d3e19bda2ab81784224
Alumno <alumno@2CI20689D1F86DB.(none)>
2014-07-29 11:13:21 -0700

    agregamos paquete que falta

--- robotitos/myro-python/instalar-myro.sh
+++ robotitos/myro-python/instalar-myro.sh
@@ -8,7 +8,7 @@ echo " - Instalamos paquetes basicos"
 apt-get -y install  git-core build-essential python-serial python-tk \
 	libsdl1.2.debian libsdl1.2-dev libsdl-image1.2 libsdl-image1.2-dev \
 	ibcurl3 libcurl3-gnutls libcurl3-nss libcurl4-nss-dev socat curl   \
-	unzip || exit 1
+	unzip idle-python || exit 1
 
 echo " - Bajamos el repositorio git de todo el software"
 git clone https://github.com/zrafa/se_uncoma.git || exit 1
