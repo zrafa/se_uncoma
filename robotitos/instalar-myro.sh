32012fad57e72aadbae34c1cff4ddb49e2608159
Juan M de la Fuente <Jmdelafuente22@users.noreply.github.com>
2018-03-14 14:18:13 -0300

    Update instalar-myro.sh

--- robotitos/instalar-myro.sh
+++ robotitos/instalar-myro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#Script para la instalación de Myro en FIDebian y Debian Testing testeado al 14-03-18.
+#El siguiente script instala las librerias necesarias para que funcione Myro correctamente, permitiendo la conexión de la computadora
+# con cualquier robot de arquitectura Frankestito, así como permite la conexión con el simulador.
 
 echo " - Chequeamos python 2.7"
 dpkg -l python2.7 || exit 1
